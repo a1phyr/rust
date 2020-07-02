670f6cc2d8b21dcef426603a12ff7de282c15d67
Benoît du Garreau <bdgdlm@outlook.com>
2020-07-02 16:52:20 +0200

    Fix tests

--- src/libcore/macros/mod.rs
+++ src/libcore/macros/mod.rs
@@ -44,12 +44,20 @@ macro_rules! assert_eq {
         match (&$left, &$right) {
             (left_val, right_val) => {
                 if !(*left_val == *right_val) {
+                    let left = stringify!($left);
+                    let right = stringify!($right);
+                    let width = if left.len() > right.len() {
+                        left.len()
+                    } else {
+                        right.len()
+                    };
+
                     // The reborrows below are intentional. Without them, the stack slot for the
                     // borrow is initialized even before the values are compared, leading to a
                     // noticeable slow down.
-                    panic!(r#"assertion failed: `(left == right)`
-  left: `{:?}`,
- right: `{:?}`"#, &*left_val, &*right_val)
+                    panic!(r#"assertion failed: `({left}) == ({right})`
+{left:>width$}: `{:?}`,
+{right:>width$}: `{:?}`"#, &*left_val, &*right_val, left=left, right=right, width=width)
                 }
             }
         }
@@ -61,13 +69,22 @@ macro_rules! assert_eq {
         match (&($left), &($right)) {
             (left_val, right_val) => {
                 if !(*left_val == *right_val) {
+                    let left = stringify!($left);
+                    let right = stringify!($right);
+                    let width = if left.len() > right.len() {
+                        left.len()
+                    } else {
+                        right.len()
+                    };
+
                     // The reborrows below are intentional. Without them, the stack slot for the
                     // borrow is initialized even before the values are compared, leading to a
                     // noticeable slow down.
-                    panic!(r#"assertion failed: `(left == right)`
-  left: `{:?}`,
- right: `{:?}`: {}"#, &*left_val, &*right_val,
-                           $crate::format_args!($($arg)+))
+                    panic!(r#"assertion failed: `({left}) == ({right})`
+{left:>width$}: `{:?}`,
+{right:>width$}: `{:?}`: {}"#, &*left_val, &*right_val,
+                           $crate::format_args!($($arg)+),
+                           left=left, right=right, width=width)
                 }
             }
         }
@@ -101,30 +118,47 @@ macro_rules! assert_ne {
         match (&$left, &$right) {
             (left_val, right_val) => {
                 if *left_val == *right_val {
+                    let left = stringify!($left);
+                    let right = stringify!($right);
+                    let width = if left.len() > right.len() {
+                        left.len()
+                    } else {
+                        right.len()
+                    };
+
                     // The reborrows below are intentional. Without them, the stack slot for the
                     // borrow is initialized even before the values are compared, leading to a
                     // noticeable slow down.
-                    panic!(r#"assertion failed: `(left != right)`
-  left: `{:?}`,
- right: `{:?}`"#, &*left_val, &*right_val)
+                    panic!(r#"assertion failed: `({left}) != ({right})`
+{left:>width$}: `{:?}`,
+{right:>width$}: `{:?}`"#, &*left_val, &*right_val, left=left, right=right, width=width)
                 }
             }
         }
     });
-    ($left:expr, $right:expr,) => {
+    ($left:expr, $right:expr,) => ({
         $crate::assert_ne!($left, $right)
-    };
+    });
     ($left:expr, $right:expr, $($arg:tt)+) => ({
         match (&($left), &($right)) {
             (left_val, right_val) => {
                 if *left_val == *right_val {
+                    let left = stringify!($left);
+                    let right = stringify!($right);
+                    let width = if left.len() > right.len() {
+                        left.len()
+                    } else {
+                        right.len()
+                    };
+
                     // The reborrows below are intentional. Without them, the stack slot for the
                     // borrow is initialized even before the values are compared, leading to a
                     // noticeable slow down.
-                    panic!(r#"assertion failed: `(left != right)`
-  left: `{:?}`,
- right: `{:?}`: {}"#, &*left_val, &*right_val,
-                           $crate::format_args!($($arg)+))
+                    panic!(r#"assertion failed: `({left}) != ({right})`
+{left:>width$}: `{:?}`,
+{right:>width$}: `{:?}`: {}"#, &*left_val, &*right_val,
+                           $crate::format_args!($($arg)+),
+                           left=left, right=right, width=width)
                 }
             }
         }
